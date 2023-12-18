c18d2ce893f40b0d215e174c342008c6bc41050d
Wojtek <wojciechsadocha@gmail.com>
2023-12-18 10:14:18 +0100

    make build

---
+++ index.56e00cd9.js
@@ -0,0 +1,2 @@
+!function e(t,n){let r=Object.keys(n),l=document.createElement("ul");for(let a of r){let r=document.createElement("li");r.textContent=a,l.append(r),e(r,n[a]),t.append(l)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.56e00cd9.js.map
